46c7eddceb80963c9255ae29fb2405177aa3ff03
arunachalamm <arunanalyst07@gmail.com>
2025-07-26 09:34:36 +0530

    Update resume_parser.py

--- resume_parser.py
+++ resume_parser.py
@@ -10,3 +10,14 @@ def extract_resume_text(file_path):
 resume_path = "E:/ResumeMentor-AI/datasets/resume_dataset/Arunachalam-Resume.pdf"
 text = extract_resume_text(resume_path)
 print(text[:1000])  # show first 1000 characters
+from skill_matcher import load_job_roles, match_skills
+from resume_parser import extract_resume_text
+
+resume_text = extract_resume_text("E:/ResumeMentor-AI/datasets/resume_dataset/Sample_resume.pdf")
+job_data = load_job_roles(r"E:\ResumeMentor-AI\datasets\job_roles.csv")  # ✅ fixed function name
+matches = match_skills(resume_text, job_data)
+for role, score, matched_skills in matches:
+    print(f"Role: {role}")
+    print(f"Match Score: {score:.2f}%")
+    print(f"Matched Skills: {matched_skills}")
+    print("="*40)
